dbb6bfb62bdfa6e691789a1f902bb502e8a57493
AmarRed07 <redzepagicamar@yahoo.com>
2020-11-19 12:07:14 -0800

    new changes

---
+++ index.js
@@ -0,0 +1,56 @@
+var coatsBrown = 10;
+var coatsBlue = 15;
+console.log (coatsBrown);
+console.log (coatsBlue);
+
+var moneyCash = 30;
+var moneyCoins = 1.50;
+console.log (moneyCash);
+console.log (moneyCoins);
+
+console.log (moneyCash - coatsBlue);
+console.log (moneyCash + moneyCoins - coatsBrown);
+
+let differenceBlue = moneyCash - coatsBlue
+    console.log ('After buying the blue coat, Sam has: $' + differenceBlue + ' left!');
+let differenceBrown = moneyCash + moneyCoins - coatsBrown
+    console.log ('After buying the brown coat, Joe has: $' + differenceBrown + ' left!');
+
+
+
+var friendsBest = 1;
+var friendsGood = 2;
+console.log (friendsBest);
+console.log (friendsGood);
+
+var ageTeen = 5;
+var ageAdult = 4;
+console.log (ageTeen);
+console.log (ageAdult);
+
+console.log (ageTeen / friendsBest);
+console.log (ageAdult / friendsGood);
+
+let teenYears = ageTeen + friendsBest
+    console.log ('Sam made ' + teenYears + ' friends during his teens.');
+let adultYears = ageAdult + friendsGood
+    console.log ('Joe made' + adultYears + ' friends during his adult years!');
+
+
+
+var nameFirst = "Sam"
+var nameFirst1 = "Joe"
+
+var nameLast = "Green"
+var nameLast1 = "Blue"
+
+var intMiddle = "L"
+var intMiddle1= "B"
+
+console.log (nameFirst + ' ' + intMiddle + ' ' + nameLast + '.');
+console.log (nameFirst1 + ' ' + intMiddle1 + ' ' + nameLast1 + '.');
+
+let fullName = nameFirst + ' ' + intMiddle + ' ' + nameLast + '.'
+    console.log ('His full name was ' + fullName);
+let fullName1 = nameFirst1 + ' ' + intMiddle1 + ' ' + nameLast1 + '.'
+    console.log ('Sam has a friend named: ' + fullName1);
